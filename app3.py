e13be22e19e96ede1bf2b57acdb590e7a0cd76ce
MAHMOODMOHAMED122 <146440584+MAHMOODMOHAMED122@users.noreply.github.com>
2026-05-10 18:56:31 +0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -523,7 +523,7 @@ with tab5:
         st.warning("⚠️ لا يوجد طلاب! أضف طالب جديد أولاً.")
 
 # التبويب 6: الحضور والملاحظات
-     elif page == "📅 الحضور والملاحظات":
+    elif page == "📅 الحضور والملاحظات":
          st.header("📅 الحضور والملاحظات")
     
     students = manager.get_all_students()
